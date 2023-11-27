898b0c901b859c1ad6d953c9d5cbcc55913e6186
kiko <kkcckkcckkcc10@gmail.com>
2023-11-27 14:54:36 -0500

    Commented code

--- DMOJ/src/SimpleEncryption.java
+++ DMOJ/src/SimpleEncryption.java
@@ -2,22 +2,26 @@ import java.util.Scanner;
 
 public class SimpleEncryption {
     public static void main(String[] args) {
-        //input
+        //take string input and turn into character array
         Scanner input = new Scanner(System.in);
         char[] keyword = input.nextLine().toCharArray();
-        //takes the message and remove all non alphabetic characters
+        //removes all non alphabetic characters before turning into array
         char[] message = input.nextLine().replaceAll("[^A-Z]+", "").toCharArray();
+        //loop though all the characters in the message
         for (int charPos = 0; charPos < message.length; charPos++){
+            //matches the character with its letter shift using modulo, then shift it using the method
             message[charPos] = shift(message[charPos],keyword[charPos % keyword.length]);
         }
+        //encoded message
         System.out.println(message);
-        
         input.close();
     }
 
+    //method that shifts letter charNum by letter shiftNum
+    //takes in char as it can be implictly casted to int
     public static char shift(int charNum, int shiftNum) {
-        System.out.println(charNum + "," + shiftNum);
         int shiftedNum = charNum + shiftNum - 65;
+        //loops back to letter A if the shifted letter is "beyond" Z
         if (shiftedNum > 90) {
             shiftedNum -= 26;
         }
