5fdb0d482aed04aeb501d249e828eed6c6c2c15a
kiko <kkcckkcckkcc10@gmail.com>
2023-11-27 13:31:27 -0500

    Solution with loops

--- DMOJ/src/SimpleEncryption.java
+++ DMOJ/src/SimpleEncryption.java
@@ -1,3 +1,17 @@
+import java.util.Scanner;
+
 public class SimpleEncryption {
-    
+    public static void main (String[] args) {
+        //input
+        Scanner input = new Scanner(System.in);
+        char[] keyword = input.nextLine().toCharArray();
+        //takes the message and remove all non alphabetic characters
+        char[] rawMessage = input.nextLine().replaceAll("[^A-Z]+", "").toCharArray();
+        int totalRow = rawMessage.length / keyword.length;
+        char[][] table = new char[keyword.length][totalRow];
+
+        for (int column = 0; column < keyword.length; column++) {
+            System.out.println(column);
+        }
+    }
 }
