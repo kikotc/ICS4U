84314b19a43339aca62ea93f3d3b53f3bc772f41
kiko <kkcckkcckkcc10@gmail.com>
2023-11-27 13:49:57 -0500

    Solution using method

--- DMOJ/src/SimpleEncryption.java
+++ DMOJ/src/SimpleEncryption.java
@@ -1,17 +1,26 @@
 import java.util.Scanner;
 
 public class SimpleEncryption {
-    public static void main (String[] args) {
+    public static void main(String[] args) {
         //input
         Scanner input = new Scanner(System.in);
         char[] keyword = input.nextLine().toCharArray();
         //takes the message and remove all non alphabetic characters
-        char[] rawMessage = input.nextLine().replaceAll("[^A-Z]+", "").toCharArray();
-        int totalRow = rawMessage.length / keyword.length;
-        char[][] table = new char[keyword.length][totalRow];
-
-        for (int column = 0; column < keyword.length; column++) {
-            System.out.println(column);
+        char[] message = input.nextLine().replaceAll("[^A-Z]+", "").toCharArray();
+        for (int charPos = 0; charPos < message.length; charPos++){
+            message[charPos] = shift(message[charPos],keyword[charPos % keyword.length]);
         }
+        System.out.println(message);
+        
+        input.close();
+    }
+
+    public static char shift(int charNum, int shiftNum) {
+        System.out.println(charNum + "," + shiftNum);
+        int shiftedNum = charNum + shiftNum - 65;
+        if (shiftedNum > 90) {
+            shiftedNum -= 26;
+        }
+        return (char) shiftedNum;
     }
 }
