cd7fcfe415a1e5f7a1a11012c082d479296056a2
kiko <kkcckkcckkcc10@gmail.com>
2023-11-28 13:10:28 -0500

    Small comment changes

--- DMOJ/src/SimpleEncryption.java
+++ DMOJ/src/SimpleEncryption.java
@@ -1,24 +1,28 @@
 import java.util.Scanner;
 
+/**
+ * @author Kiko C
+ * @version 2.0 - Uses a modulo approach to determine the letter shift, instead of nested for loops, which was quickly scrapped
+ * 
+ * Solves the CCC '04 J4 - Simple Encryption problem (https://dmoj.ca/problem/ccc04j4)
+ */
+
 public class SimpleEncryption {
     public static void main(String[] args) {
-        //take string input and turn into character array
         Scanner input = new Scanner(System.in);
         char[] keyword = input.nextLine().toCharArray();
         //removes all non alphabetic characters before turning into array
         char[] message = input.nextLine().replaceAll("[^A-Z]+", "").toCharArray();
-        //loop though all the characters in the message
+        //loop though characters in the message
         for (int charPos = 0; charPos < message.length; charPos++){
-            //matches the character with its letter shift using modulo, then shift it using the method
+            //matches the character with its letter shift (column) using modulo, then shift it using the method
             message[charPos] = shift(message[charPos],keyword[charPos % keyword.length]);
         }
-        //encoded message
         System.out.println(message);
         input.close();
     }
 
-    //method that shifts letter charNum by letter shiftNum
-    //takes in char as it can be implictly casted to int
+    //method that shifts letter by a letter shift
     public static char shift(int charNum, int shiftNum) {
         int shiftedNum = charNum + shiftNum - 65;
         //loops back to letter A if the shifted letter is "beyond" Z
