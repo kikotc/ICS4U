3941c367d66d75e9284c8d40f9db4edb38c10194
kiko <147737506+kikotc@users.noreply.github.com>
2024-02-01 22:01:21 -0500

    Working sort

--- DMOJ/src/AbsolutelyAcidic.java
+++ DMOJ/src/AbsolutelyAcidic.java
@@ -1,38 +1,32 @@
 import java.util.*;
 
 public class AbsolutelyAcidic {
-    public static void main (String[] args) {
-        Scanner input = new Scanner(System.in);
-       
-        int numberOfSensors = input.nextInt();
-        short[] readings = new short[numberOfSensors];
-        for (int readingNum = 0; readingNum < numberOfSensors; readingNum++) {
-            readings[readingNum] = input.nextShort();
-        }
+	public static void main (String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		//uses amount of readings to make an array
+		int numberOfSensors = input.nextInt();
+		short[] readings = new short[numberOfSensors + 1];
+		//turn all readings into an array
+		for (int readingNum = 0; readingNum < numberOfSensors; readingNum++) {
+			readings[readingNum] = input.nextShort();
+		}
+		readings[numberOfSensors] = 1001;
 
-        ArrayList<Integer> sorted = new ArrayList<Integer>();
+		Arrays.sort(readings);
+		HashMap<Integer,Integer> sorted = new HashMap<Integer,Integer>();
 
-        Arrays.sort(readings);
-        int occurances = 0;
-        int lastOccurances = 0;
-        boolean twoFrequencies = false;
-        for (int readingNum = 0; readingNum < numberOfSensors - 1; readingNum++) {
-            if (readings[readingNum] == readings[readingNum + 1]) {
-                occurances++;
-            //multiple one frequency
-            } else if ((lastOccurances == occurances) && sorted.size() != 1) {
-                sorted.add(Integer.valueOf(readings[readingNum]));
-                lastOccurances = occurances;
-                occurances = 0;
-            } else if (sorted.size() == 1) {
-                twoFrequencies = true;
-                sorted.add(Integer.valueOf(readings[readingNum]));
-                lastOccurances = occurances;
-                occurances = 0;
-            }
-        }
-        
-        System.out.println(numberOfSensors + ": " + Arrays.toString(readings));
-        System.out.println(sorted);
-    }
+		int occurances = 1;
+		for (int readingNum = 0; readingNum < numberOfSensors; readingNum++) {
+			if (readings[readingNum] == readings[readingNum + 1]) {
+				occurances++;
+			} else {
+				sorted.put(Integer.valueOf(readings[readingNum]), occurances);
+				occurances = 1;
+			}
+		}
+		
+		System.out.println(numberOfSensors + ": " + Arrays.toString(readings));
+		System.out.println(sorted);
+	}
 }
