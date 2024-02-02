48502a8c1ff4be533052684e8021ebc11e99332d
kiko <kkcckkcckkcc10@gmail.com>
2024-02-02 18:31:09 -0500

    Formatting

--- DMOJ/src/AbsolutelyAcidic.java
+++ DMOJ/src/AbsolutelyAcidic.java
@@ -2,21 +2,21 @@ import java.util.*;
 import java.util.Map.Entry;
 
 public class AbsolutelyAcidic {
-	public static void main (String[] args) {
+	public static void main(String[] args) {
 		Scanner input = new Scanner(System.in);
-		
-		//uses amount of readings to make an array
+
+		// uses amount of readings to make an array
 		int numberOfSensors = input.nextInt();
 		short[] readings = new short[numberOfSensors + 1];
-		//turn all readings into an array
+		// turn all readings into an array
 		for (int readingNum = 0; readingNum < numberOfSensors; readingNum++) {
 			readings[readingNum] = input.nextShort();
 		}
-        input.close();
+		input.close();
 		readings[numberOfSensors] = 1001;
 
 		Arrays.sort(readings);
-		HashMap<Integer,Integer> counted = new HashMap<Integer,Integer>();
+		HashMap<Integer, Integer> counted = new HashMap<Integer, Integer>();
 
 		int occurances = 1;
 		for (int readingNum = 0; readingNum < numberOfSensors; readingNum++) {
@@ -28,19 +28,64 @@ public class AbsolutelyAcidic {
 			}
 		}
 
-        int highestFrequency = 0;
-        int secondFrequency = 0;
+		int firstFrequency = 0;
+		int secondFrequency = 0;
 
-        for (Integer value : counted.values()) {
-            if (value > highestFrequency) {
-                highestFrequency = value;
-            } else if (value > secondFrequency) {
-                secondFrequency = value;
-            }
-        }
+		for (Integer value : counted.values()) {
+			if (value > firstFrequency) {
+				firstFrequency = value;
+			} else if (value > secondFrequency) {
+				secondFrequency = value;
+			}
+		}
 
-		System.out.println(numberOfSensors + ": " + Arrays.toString(readings));
-		System.out.println(counted);
-        System.out.println(highestFrequency + ", " + secondFrequency);
+		int difference = 0;
+
+		if (firstFrequency == secondFrequency) {
+			short lowestValue = 1001;
+			short highestValue = 0;
+			for (Map.Entry<Integer, Integer> entry : counted.entrySet()) {
+				if (entry.getValue() == firstFrequency) {
+					short key = entry.getKey().shortValue();
+					if (key < lowestValue) {
+						lowestValue = key;
+					} else if (key > highestValue) {
+						highestValue = key;
+					}
+				}
+			}
+			difference = highestValue - lowestValue;
+		} else {
+			if (secondFrequency > firstFrequency) {
+				int temp = firstFrequency;
+				firstFrequency = secondFrequency;
+				secondFrequency = temp;
+			}
+			short lowestValue = 1001;
+			short highestValue = 0;
+			short firstValue = 0;
+			short key;
+			for (Map.Entry<Integer, Integer> entry : counted.entrySet()) {
+				key = entry.getKey().shortValue();
+				if (entry.getValue() == firstFrequency) {
+					firstValue = key;
+				}
+				if (entry.getValue() == secondFrequency) {
+					if (key < lowestValue) {
+						lowestValue = key;
+					} else if (key > highestValue) {
+						highestValue = key;
+					}
+				}
+			}
+			difference = Math.max(Math.abs(firstValue - lowestValue), Math.abs(firstValue - highestValue));
+		}
+
+		/*
+		 * System.out.println(numberOfSensors + ": " + Arrays.toString(readings));
+		 * System.out.println(counted);
+		 * System.out.println(firstFrequency + ", " + secondFrequency);
+		 */
+		System.out.println(difference);
 	}
 }
