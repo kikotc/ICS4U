e6f4b6c37278e11bd1aa2e7eec8037540f38fdf5
kiko <kkcckkcckkcc10@gmail.com>
2024-02-02 10:14:57 -0500

    Sorted the occurances

--- DMOJ/src/AbsolutelyAcidic.java
+++ DMOJ/src/AbsolutelyAcidic.java
@@ -1,4 +1,5 @@
 import java.util.*;
+import java.util.Map.Entry;
 
 public class AbsolutelyAcidic {
 	public static void main (String[] args) {
@@ -11,22 +12,38 @@ public class AbsolutelyAcidic {
 		for (int readingNum = 0; readingNum < numberOfSensors; readingNum++) {
 			readings[readingNum] = input.nextShort();
 		}
+        input.close();
 		readings[numberOfSensors] = 1001;
 
 		Arrays.sort(readings);
-		HashMap<Integer,Integer> sorted = new HashMap<Integer,Integer>();
+		HashMap<Integer,Integer> counted = new HashMap<Integer,Integer>();
+        LinkedHashMap<Integer, Integer> sorted = new LinkedHashMap<>();
+        ArrayList<Integer> list = new ArrayList<>();
 
 		int occurances = 1;
 		for (int readingNum = 0; readingNum < numberOfSensors; readingNum++) {
 			if (readings[readingNum] == readings[readingNum + 1]) {
 				occurances++;
 			} else {
-				sorted.put(Integer.valueOf(readings[readingNum]), occurances);
+				counted.put(Integer.valueOf(readings[readingNum]), occurances);
 				occurances = 1;
 			}
 		}
+
+        for (Map.Entry<Integer, Integer> entry : counted.entrySet()) {
+            list.add(entry.getValue());
+        }
+        Collections.sort(list, Collections.reverseOrder());
+        for (int num : list) {
+            for (Entry<Integer, Integer> entry : counted.entrySet()) {
+                if (entry.getValue().equals(num)) {
+                    sorted.put(entry.getKey(), num);
+                }
+            }
+        }
 		
 		System.out.println(numberOfSensors + ": " + Arrays.toString(readings));
-		System.out.println(sorted);
+		System.out.println(counted);
+        System.out.println(list);
 	}
 }
